8e0c1613bc7cdcf138491cb46ef73614633091c5
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2024-02-22 01:10:27 +0100

    working on the about page

--- app/about/page.js
+++ app/about/page.js
@@ -2,34 +2,36 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className=" flex flex-col items-center justify-center">
-      <span className="font-serif text-2xl md:text-3xl lg:text-4xl font-medium text-center">
+    <div className="flex flex-col items-center justify-center mt-8">
+      <span className="font-serif text-3xl md:text-3xl lg:text-4xl font-medium text-center py-6 pb-8 ">
         About
       </span>
       <div className="flex flex-col">
         <span>
           Tekkd Solutions is a comprehensive professional services company
-          catering to a diverse clientele, from homeowners to commercial
-          developers. We provide a wide range of services to help you with your
-          project, from start to finish.
+          catering to a diverse clientele, ranging from different industries. We
+          provide a wide range of services to help you with your project, from
+          start to finish.
         </span>
+        <span>Our Services</span>
         <span>
-          Our Services: Project Management: Our experienced team of project
-          managers will ensure that your project is delivered on time and on
-          budget. We use efficient planning, organization, and execution to
-          bring your projects to life. Consulting: We offer expert advice and
-          strategic guidance to help you make informed decisions about your
-          business. Our consulting services cover a wide range of areas,
-          including IT, marketing, and finance. Software and Web Development:
-          Our development team can create cutting-edge software and web
-          solutions tailored to your specific needs. We specialize in creating
-          scalable and user-friendly applications that will help you achieve
-          your business goals. Digital/Social Media Marketing: We can help you
-          reach and engage your target audience with our comprehensive digital
-          and social media marketing services. We will help you grow your brand
-          awareness and drive sales. Hardware Supply and Maintenance: We can
-          supply you with the hardware you need and provide ongoing maintenance
-          to ensure that your systems are running smoothly.
+          Project Management: Our experienced team of project managers will
+          ensure that your project is delivered on time and on budget. We use
+          efficient planning, organization, and execution to bring your projects
+          to life. <br /> Consulting: We offer expert advice and strategic
+          guidance to help you make informed decisions about your business. Our
+          consulting services cover a wide range of areas, including IT,
+          marketing, and finance.
+          <br /> Software and Web Development: Our development team can create
+          cutting-edge software and web solutions tailored to your specific
+          needs. We specialize in creating scalable and user-friendly
+          applications that will help you achieve your business goals. <br />
+          Digital/Social Media Marketing: We can help you reach and engage your
+          target audience with our comprehensive digital and social media
+          marketing services. We will help you grow your brand awareness and
+          drive sales. <br /> Hardware Supply and Maintenance: We can supply you
+          with the hardware you need and provide ongoing maintenance to ensure
+          that your systems are running smoothly.
         </span>
         <span>
           Why Choose Tekkd Solutions? We have a team of experienced
