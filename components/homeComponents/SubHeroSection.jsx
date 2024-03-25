a2840a59e09bc53aa4da0c0b859a83e57e205cc7
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-03-25 01:05:41 +0100

    Final changes

--- components/homeComponents/SubHeroSection.jsx
+++ components/homeComponents/SubHeroSection.jsx
@@ -8,7 +8,7 @@ const SubHeroSection = () => {
       <div className="flex flex-col items-center justify-center gap-2">
         <span className="text-4xl h-6 text-gray-400">*</span>
         <h1 className=" font-medium w-300 text-center text-2xl sm:text-3xl  ">
-          A passion for creating spaces
+          A passion for digital technology solutions to world problems
         </h1>
       </div>
       <p className="  text-gray-600 text-center w-[360px] sm:w-[480px] md:w-[550px] ">
@@ -16,7 +16,7 @@ const SubHeroSection = () => {
         clientele, ranging from homeowners to commercial developers.
       </p>
 
-      <div className=" flex flex-wrap  items-center justify-center gap-8 mt-4">
+      <div className=" flex flex-wrap items-center justify-center gap-8 mt-4">
         {contentData.map((content, index) => (
           <Desc
             key={index}
