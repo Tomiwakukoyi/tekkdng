74b9cd65be73d7ade8cf258dec456997c64e8f40
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-09-24 23:11:28 +0100

    updated npm and continuing with adding motion and few tweaks

--- components/portfolio/Clients.jsx
+++ components/portfolio/Clients.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
-
-const Clients = ({sector, image}) => {
+const Clients = ({ sector, image }) => {
   return (
-    <section className=' flex flex-col items-center justify-center mx-auto bg-gray-200 w-fit p-3 rounded-2xl gap-4'>
-       <Image
+    <motion.section
+      className="flex flex-col items-center justify-center bg-gray-200 w-60 h-60 p-6 lg:py-7 rounded-2xl gap-3 shadow-md"
+      initial={{ opacity: 0, scale: 0.9 }} // Initial state
+      animate={{ opacity: 1, scale: 1 }} // Animation to apply
+      transition={{ duration: 0.5 }} // Duration of the animation
+    >
+      <Image
         src={`/images/${image}`}
-        width={300}
-        height={300}
-        className=" rounded-2xl"
+        width={200}
+        height={200}
+        className="rounded-2xl object-cover h-full w-full"
       />
-        <span className=' font-medium'>{sector}</span>
-    </section>
-  )
-}
+      <span className="font-medium text-center">{sector}</span>
+    </motion.section>
+  );
+};
 
-export default Clients
+export default Clients;
