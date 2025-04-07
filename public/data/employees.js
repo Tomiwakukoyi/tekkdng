131cc03f980910e9617f5e5e1276867958184d0c
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2025-04-07 11:29:46 +0100

    changes

--- public/data/employees.js
+++ public/data/employees.js
@@ -1,17 +1,17 @@
 // employees.js
 const employees = [
   {
-    name: "Mr. Kukoyi Tomiwa",
+    name: "Mr. Kukoyi Oluwatomiwa",
     position: "CEO/CTO",
     picture: "Tomi.PNG",
   },
   {
-    name: "Mr. Kaiyewu Tomiwa",
+    name: "Mr. Kaiyewu AyoOluwatomiwa",
     position: "Chief Innovations Officer",
     picture: "tommy2.JPG",
   },
   {
-    name: "Mr. Adeleke Seun",
+    name: "Mr. Adeleke Oluwaseun",
     position: "Chief Operations Officer",
     picture: "seun.jpg",
   },
